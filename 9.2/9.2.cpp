f347300e9f3661e41837361cacf3f1d99df9cfd8
Alex <alexprokopov@gmail.com>
2024-01-14 12:38:06 +0200

    repeat

--- 9.2/9.2.cpp
+++ 9.2/9.2.cpp
@@ -1,55 +1,67 @@
-﻿// 9.1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿// 9.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 #include <string>
+
+
+struct Employee
+{
+    short id;
+    int age;
+    double salary;
+    void show_salary()
+    {
+        std::cout << "Salary: " << salary << std::endl;
+    }
+};
+
+
+void showVal(const int val)
+{
+    std::cout << val << std::endl;
+}
 
 int main()
 {
-    std::vector<int> vec = {1,8,3,4,13,6,7,13,9,15};
-    std::cout << vec.size() << std::endl;
-    vec.push_back(11);
-    std::cout << vec.size() << std::endl;
-    for (auto el : vec)
-    {
-        std::cout << el << " ";
-    }
-    std::cout << std::endl;
-    //std::cout << vec[5] << std::endl;
-    vec.insert(vec.begin()+3, 12);
-    for (auto el : vec)
-    {
-        std::cout << el << " ";
-    }
-    std::cout << std::endl;
-    vec.erase(vec.begin() + 3);
-    for (auto el : vec)
-    {
-        std::cout << el << " ";
-    }
-    std::cout << std::endl;
-    sort(vec.begin(), vec.end());
-    for (auto el : vec)
-    {
-        std::cout << el << " ";
-    }
-    std::cout << std::endl;
+    int a = 1;
+    unsigned int b = 2;
+    int8_t a8;
+    int16_t a16;
+    int32_t a32;
+    uint8_t ua8;
+    uint16_t ua16;
+    uint32_t ua32;
+    bool val = true;
+    char symb = 'A';
+    float fval = 12.5; //	От ±1,5 x 10E−45 до ±3,4 x 10E38
+    double dval = 12.5;//   от ±5,0 × 10E−324 до ±1,7 × 10E308
+    long double ldval = 12.5;
+    std::string stroka = "Hello";
+    Employee somebody = { 1, 15, 12.5 };
+    std::cout << somebody.id << " " << somebody.age << " " << somebody.salary << std::endl;
+    somebody.show_salary();
 
-    std::vector<std::string> stroki = { "adcc", "abcc", "aaac", "aaaa" };
-    sort(stroki.begin(), stroki.end());
-    for (auto el : stroki)
-    {
-        std::cout << el << " ";
-    }
+    int arr1[5] = { 1,2,3,4,5 };
+    int* arr2 = new int[5];
+
+    for (int i = 0; i < 5; i++) arr2[i] = i + 1;
+    for (int i = 0; i < 5; i++) std::cout << arr2[i] << " ";
     std::cout << std::endl;
+    delete[] arr2;
+    //for (int i = 0; i < 5; i++) std::cout << arr2[i] << " ";
+    //std::cout << std::endl;
 
-    std::vector<int> v{ 3, 1, -4, 1, 5, 9 };
-    std::vector<int>::iterator result = std::min_element(v.begin(), v.end());
-    std::cout << "min element has value " << *result << " and index [" << std::distance(v.begin(), result) << "]\n";
+    int* dynVal = new int;
+    *dynVal = 5;
+    std::cout << *dynVal << std::endl;
+    //delete dynVal;
+    //std::cout << *dynVal << std::endl;
 
+    int vv = 15;
+    showVal(vv);
 }
+
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
