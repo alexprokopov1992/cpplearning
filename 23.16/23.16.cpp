15f6d54f33fae60eec4551fa661ce3b7a552fdf0
Alex <alexprokopov@gmail.com>
2024-04-22 18:35:28 +0300

    commit

---
+++ 23.16/23.16.cpp
@@ -0,0 +1,58 @@
+﻿#include <iostream>
+
+class SomeString
+{
+private:
+    char* m_data;
+    int m_length;
+
+public:
+    SomeString(const char* data = "", int length = 0) :
+        m_length(length)
+    {
+        if (!length)
+            m_data = nullptr;
+        else
+            m_data = new char[length];
+
+        for (int i = 0; i < length; ++i)
+            m_data[i] = data[i];
+    }
+
+
+    SomeString& operator= (const SomeString& str);
+
+    friend std::ostream& operator<<(std::ostream& out, const SomeString& s);
+};
+
+std::ostream& operator<<(std::ostream& out, const SomeString& s)
+{
+    out << s.m_data;
+    return out;
+}
+
+// Перегрузка оператора присваивания (плохой вариант перегрузки)
+SomeString& SomeString::operator= (const SomeString& str)
+{
+    // Если m_data уже имеет значение, то удаляем это значение
+    if (m_data) delete[] m_data;
+
+    m_length = str.m_length;
+
+    // Копируем значение из str в m_data неявного объекта
+    m_data = new char[str.m_length];
+    for (int i = 0; i < str.m_length; ++i)
+        m_data[i] = str.m_data[i];
+
+    // Возвращаем текущий объект
+    return *this;
+}
+
+int main()
+{
+    SomeString anton("Anton", 7);
+    anton = anton;
+    std::cout << anton;
+
+    return 0;
+}
---
+++ 23.17/23.17.cpp
@@ -0,0 +1,99 @@
+﻿#include <cstring> // для strlen()
+#include <cassert> // для assert()
+#include <iostream>
+
+class SomeString
+{
+private:
+    char* m_data;
+    size_t m_length;
+
+public:
+    SomeString(const char* source = "")
+    {
+        assert(source); // проверяем не является ли source нулевой строкой
+
+        // Определяем длину source + еще один символ для нуль-терминатора (символ завершения строки)
+        m_length = strlen(source) + 1;
+
+        // Выделяем достаточно памяти для хранения копируемого значения в соответствии с длиной этого значения
+        m_data = new char[m_length];
+
+        // Копируем значение по символам в нашу выделенную память
+        for (int i = 0; i < m_length; ++i)
+            m_data[i] = source[i];
+
+        // Убеждаемся, что строка завершена
+        m_data[m_length - 1] = '\0';
+    }
+
+    ~SomeString() // деструктор
+    {
+        // Освобождаем память, выделенную для нашей строки
+        delete[] m_data;
+    }
+    SomeString(const SomeString& source);
+    SomeString& operator=(const SomeString& source);
+    char* getString() { return m_data; }
+    size_t getLength() { return m_length; }
+};
+
+SomeString::SomeString(const SomeString& source)
+{
+    // Поскольку m_length не является указателем, то мы можем выполнить поверхностное копирование
+    m_length = source.m_length;
+
+    // m_data является указателем, поэтому нам нужно выполнить глубокое копирование, при условии, что этот указатель не является нулевым
+    if (source.m_data)
+    {
+        // Выделяем память для нашей копии
+        m_data = new char[m_length];
+
+        // Выполняем копирование
+        for (int i = 0; i < m_length; ++i)
+            m_data[i] = source.m_data[i];
+    }
+    else
+        m_data = 0;
+}
+
+// Оператор присваивания
+SomeString& SomeString::operator=(const SomeString& source)
+{
+    // Проверка на самоприсваивание
+    if (this == &source)
+        return *this;
+
+    // Сначала нам нужно очистить предыдущее значение m_data (члена неявного объекта)
+    delete[] m_data;
+
+    // Поскольку m_length не является указателем, то мы можем выполнить поверхностное копирование
+    m_length = source.m_length;
+
+    // m_data является указателем, поэтому нам нужно выполнить глубокое копирование, при условии, что этот указатель не является нулевым
+    if (source.m_data)
+    {
+        // Выделяем память для нашей копии
+        m_data = new char[m_length];
+
+        // Выполняем копирование
+        for (int i = 0; i < m_length; ++i)
+            m_data[i] = source.m_data[i];
+    }
+    else
+        m_data = 0;
+
+    return *this;
+}
+
+int main()
+{
+    SomeString hello("Hello, world!");
+    {
+        SomeString copy = hello; // используется конструктор копирования по умолчанию
+    } // объект copy является локальной переменной, которая уничтожается здесь. Деструктор удаляет значение-строку объекта copy, оставляя, таким образом, hello с висячим указателем
+
+    std::cout << hello.getString() << '\n'; // здесь неопределенные результаты
+
+    return 0;
+}
